40722eeb34656a51590216aee4194130e9e1e1bf
Kengo Yamada <yamada.k03@gmail.com>
2024-05-15 00:36:12 +0900

    [change] リンク

--- app/kuma/page.tsx
+++ app/kuma/page.tsx
@@ -106,9 +106,9 @@ export default async function Kuma({ params, searchParams }: Props) {
                 {/* <div className="px-4 md:px-20 ">コラム一覧</div> */}
 
                 <div className="mb-4 flex items-center justify-center gap-2 text-sm">
-                    <a href="/kuma/about" className="text-blue-800 font-bold">運営者情報</a>
+                    <a href="/kuma/about" target="_blank" rel="noopener noreferrer" className="text-blue-800 font-bold">運営者情報</a>
                     <div>/</div>
-                    <a href="/kuma/info" className="text-blue-800 font-bold">調査概要</a>
+                    <a href="/kuma/info" target="_blank" rel="noopener noreferrer" className="text-blue-800 font-bold">調査概要</a>
                 </div>
 
                 <div className="pb-4 flex items-center justify-center gap-2 text-xs text-gray-400">
